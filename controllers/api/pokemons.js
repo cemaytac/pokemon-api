ec50cde8106ec22033a46a74c6f28700754b8904
cem aytac <cenko@MacBook-Air-3.local>
2020-08-27 15:21:33 -0400

    added routes and controllers

---
+++ controllers/api/pokemons.js
@@ -0,0 +1,29 @@
+const Pokemon = require('../../models/pokemon')
+
+module.exports = {
+  index,
+  show,
+  create,
+  update,
+  delete: deleteOne
+}
+
+function index(req, res) {
+
+}
+
+function show(req, res) {
+
+}
+
+function create(req, res) {
+
+}
+
+function update(req, res) {
+
+}
+
+function deleteOne(req, res) {
+
+}
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const pokemonCtrl = require('../controllers/api/pokemons');
+
+router.get('/pokemon', pokemonCtrl.index)
+router.get('/pokemon/:id', pokemonCtrl.show)
+router.post('/pokemon', pokemonCtrl.create)
+router.put('/pokemon/:id', pokemonCtrl.update)
+router.delete('/pokemon/:id', pokemonCtrl.delete)
 
 module.exports = router;
